db9069aecd7ea600fd7fc997dab5f43cded15b24
Renzo Kooi <renzo.kooi@gmail.com>
2024-04-21 18:07:55 +0200

    b&b

--- index.js
+++ index.js
@@ -1,48 +1,22 @@
-/**
- * Work In Progress
- * ----------------------------------------------------------------------
- * The module delivers two ways to create an Enum:
- * ENUM(...values)  => writable (can add or remove values), default
- * ENUM$(...values) => readonly
- *
- * Value retrieval is case insensitive
- *   e.g. myEnum.somevalue, myEnum.SOMEVALUE
- * Keys must conform the rules for ES variables
- *
- * To query a value against multiple values
- * one can use either
- *  myEnum.has(value, myEnum.Something|myEnum.somethingelse),
- *  myEnum.is(value, myEnum.Something|myEnum.somethingelse) or
- *  myEnum[`something` + `$is`](myEnum.Something|myEnum.somethingelse)
- *
- * to retrieve the actual (initial) key for a value use
- *  myEnum.keyFor(myEnum.SOMEVALUE)
- *
- * to retrieve an array of current keys in an enum use
- *   myEnum.keys
- * ----------------------------------------------------------------------
- */
-const ENUM = EnumFactory();
-const ENUM$ = EnumFactory(true);
-
+const [ENUM, ENUM$] = [EnumFactory(), EnumFactory(true)];
 export { ENUM as default, ENUM$ };
 
 function EnumFactory(readOnly = false) {
   const enumTrap = retrieveEnumTrap(readOnly);
-
-  return function ENUM({ name = 'Anonymous instance', values = [] } = {}) {
+  
+  return function({ name = 'Anonymous instance', values = [] } = {}) {
     let i = 0;
     const Enum = {
       None: BigInt(0),
       [Symbol.for(`name`)]: `ENUM${readOnly ? `$` : ``} ${name}`,
       [Symbol.for(`proxied`)]: `Enum`,
     };
-
+    
     for (const value of values) {
       const name = maybeName(value, Enum);
       i = name ? addFlag(name, Enum, i) : i;
     }
-
+    
     return new Proxy(readOnly ? Object.freeze(Enum) : Enum, enumTrap);
   };
 }
@@ -51,11 +25,11 @@ function addFlag(name, Enum, i) {
   const value = 1n << BigInt(i);
   Enum[name] = value;
   Enum[`${name}$is`] = (subset) => valueIn(value, subset);
-
+  
   return i + 1;
 }
 
-function retrieveEnumTrap(readOnly, name) {
+function retrieveEnumTrap(readOnly) {
   const traps = {
     add: (target, readOnly) => add(target, readOnly),
     keyfor: (target) => key4Value(target),
@@ -64,92 +38,82 @@ function retrieveEnumTrap(readOnly, name) {
     remove: (target, readOnly) => remove(target, readOnly),
     keys: (target) => Object.keys(target).filter((k) => !/\$/.test(k)),
   };
+  
+  return trapMe(traps, readOnly)
+}
 
-  const notKeysRE = RegExp(
-    `symbol|prototype|(^${Object.keys(traps).join(`|`)}|constructor|tostring|valueof)$`, `i`);
-
-  const enumTrap = {
+function trapMe(traps, readOnly) {
+  const notKeysRE = RegExp(`symbol|prototype|(^${
+    Object.keys(traps).join(`|`)}|constructor|tostring|valueof)$`, `i`);
+  
+  return {
     get(target, key) {
-      if (typeof key === `symbol` && !/name/i.test(String(key))) {
-        return target[key];
+      if (typeof key === `symbol`) {
+        return /\(name\)$/i.test(String(key)) ? target[Symbol.for(`name`)] : target[key];
       }
-
-      if (String(key) === Symbol.for(`name`)) {
-        return target[Symbol.for(`name`)];
-      }
-
+      
       let key_lc = String(key).trim().toLowerCase();
       let method = key_lc.split(/[._]/)?.[1]?.toUpperCase();
       const value = retrieveEnumValue(target, key_lc, key, notKeysRE);
-
+      
       return method in traps
         ? traps[method](value)
         : key_lc in traps
-        ? traps[key_lc](target, readOnly)
-        : value;
+          ? traps[key_lc](target, readOnly)
+          : value;
     },
   };
-
-  return enumTrap;
 }
 
 function add(target, readOnly) {
-  return (value) => {
-    value = String(value).trim();
+  return key => {
+    key = String(key).trim();
     const enumName = `${target[Symbol.for(`name`)]}`;
-
-    if (readOnly) {
-      return warn(
-        `${enumName} is readonly. [add("${value}")] not available.`,
-        () => {}
-      );
-    }
-
-    if (getKeyValue(target)(value)) {
-      return warn(`${enumName} add: "${value}" exists, not added`);
-    }
-
-    if (isInvalid(value)) {
-      return warn(`${enumName} add: "${value}" invalid key value`);
-    }
-
-    return addFlag(value, target, Object.values(target).length);
+    return readOnly
+      ? warn(
+        `${enumName} is readonly. [add("${key}")] not available.`,
+        () => {} )
+      : getKeyValue(target)(key)
+        ? warn(`${enumName} add: "${key}" exists, not added`)
+        : isInvalid(key)
+          ? warn(`${enumName} add: "${key}" invalid key value`)
+          : addFlag(key, target, Object.values(target).length);
   };
 }
 
 function remove(target, readOnly) {
-  return (key) => {
+  return key => {
     const enumName = `${target[Symbol.for(`name`)]}`;
-
+    
     if (readOnly) {
       return warn(
         `${enumName} is readonly. [remove("${key}")] not available.`,
         () => {}
       );
     }
-
+    
     const realKey = getKeyValue(target)(String(key));
-
+    
     if (!realKey) {
       return warn(`${enumName} remove: key "${key}" does not exist`);
     }
-
+    
     delete target[realKey];
     reFlag(target);
   };
 }
 
 function reFlag(target) {
-  const entries = Object.entries(target);
-
-  for (let i = 0; i < entries; i += 1) {
-    const [key] = entries[i][0];
-
+  const keys = Object.keys(target);
+  
+  for (let i = 0; i < keys; i += 1) {
+    const [key] = keys[i];
+    
     if (/\$/.test(key)) {
       delete target[key];
       continue;
     }
-
+    
     if (key !== `None`) {
       delete target[key];
       addFlag(key, target, i);
@@ -180,9 +144,7 @@ function warn(warning, returnValue) {
 
 function getKeyValue(target) {
   return (forKey) =>
-    Object.keys(target).find(
-      (key) => key.toLowerCase() === forKey.toLowerCase()
-    );
+    Object.keys(target).find( (key) => key.toLowerCase() === forKey.toLowerCase() );
 }
 
 function getTargetFor(target, key) {
@@ -192,25 +154,19 @@ function getTargetFor(target, key) {
 function retrieveEnumValue(target, key, originalKey, notKeysRE) {
   key = String(key).trim();
   const warnNoKey = (invalid, rv) =>
-    warn(
-      `${enumName}[${originalKey}${
-        invalid ? ` (invalid!)` : ``
-      }] does not exist`,
-      rv ?? target.None
-    );
+    warn( `${enumName}[${originalKey}] ${
+      invalid ? `is invalid and thus ` : ``}does not exist`, rv ?? target.None );
   const invalid = isInvalid(key);
   const enumName = target[Symbol.for('name')];
   const hasKey = getKeyValue(target)(key) || false;
-
-  if (!hasKey && /\$is$/i.test(key)) {
-    return warnNoKey(invalid, (subset) => valueIn(target.None, subset));
-  }
-
-  if (!notKeysRE.test(key) && !hasKey) {
-    return warnNoKey(invalid);
-  }
-
-  return hasKey ? getTargetFor(target, key) : target.None;
+  
+  return !hasKey && /\$is$/i.test(key)
+    ? warnNoKey(invalid, (subset) => valueIn(target.None, subset))
+    : !notKeysRE.test(key) && !hasKey
+      ? warnNoKey(invalid)
+      : hasKey
+        ? getTargetFor(target, key)
+        : target.None;
 }
 
 function key4Value(target) {
@@ -222,7 +178,7 @@ function key4Value(target) {
         () => undefined
       );
     }
-
+    
     const found = Object.entries(target).find(([_, val]) => val === value);
     return (found && found[0]) || undefined;
   };
@@ -230,13 +186,15 @@ function key4Value(target) {
 
 function maybeName(value, target) {
   value = String(value);
-
+  
   return isInvalid(value)
     ? warn(
-        `${
-          target[Symbol.for(`name`)]
-        } on create: "${value}" not valid, omitted`,
-        false
-      )
+      `${target[Symbol.for(`name`)]} on create: "${value}" not valid, omitted`,
+      false
+    )
     : value.trim();
+}
+
+function pipe(...functions) {
+  return initial => functions.reduce((param, func) => func(param), initial);
 }
