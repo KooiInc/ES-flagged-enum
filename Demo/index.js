e97cc2272f0f551527d6c3afd8d0a501968025c5
Renzo Kooi <renzo.kooi@gmail.com>
2024-05-07 14:29:29 +0200

    Redo backlink
    (wft)

--- Demo/index.js
+++ Demo/index.js
@@ -25,6 +25,10 @@ function runDemo() {
     | <a target="_blank" href="https://github.com/KooiInc/ES-flagged-enum">@Github</a></p>`);
   }
   
+  if (/github/i.test(location.href)) {
+    print(`!!<p><a target="_top" href="https://github.com/KooiInc/ES-flagged-enum">Back to repository</a></p>`);
+  }
+  
   print(
     `!!British weekday names example`,
     `!!<code class="block">${appText.initialize}</code>`);
